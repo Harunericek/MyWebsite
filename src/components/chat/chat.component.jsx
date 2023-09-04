20bb7d2ca02698b8435a857faaceeb08874210dd
RagnaLothbrok <77488069+Harunericek@users.noreply.github.com>
2023-09-04 08:25:02 +0000

    Update chat.component.jsx and chat.styles.css

--- src/components/chat/chat.component.jsx
+++ src/components/chat/chat.component.jsx
@@ -1,5 +1,6 @@
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import SendIcon from '@mui/icons-material/Send';
+import CloseIcon from '@mui/icons-material/Close';
 import { useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import "./chat.styles.css"
@@ -31,15 +32,20 @@ const handleSend = () => {
             (<div onClick={handleClick} className="chat-bubble-container-mobile">
                 <ChatBubbleOutlineIcon className="chat-bubble" style={{ fontSize: "33px"}}/>
             </div>)}
-{state && (        <div className="chat-container">
-           <div className="chat-header"></div>
-           <div className="chat-body"></div>
-           <div className="chat-input">
-            <div onClick={handleSend} className='send-icon-container'>
-                <SendIcon style={{fontSize: "30px", color: "white", margin: "auto" }} />
-            </div>
-            <textarea className='input-field' />
-           </div>
+        {state && (
+            <div className="chat-container">
+                <div className="chat-header">
+                    <div onClick={handleClick} className="closeItem-container">
+                        <CloseIcon style={{color: "white", fontSize: "30px"}} />
+                    </div>
+                </div>
+                <div className="chat-body"></div>
+                <div className="chat-input">
+                <div onClick={handleSend} className='send-icon-container'>
+                    <SendIcon style={{fontSize: "30px", color: "white", margin: "auto" }} />
+                </div>
+                    <textarea className='input-field' />
+                </div>
         </div>)}
     </div>
     )
