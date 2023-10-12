74116e0c69639881e554316b4a9b0fe95a62c731
RagnaLothbrok <77488069+Harunericek@users.noreply.github.com>
2023-10-12 15:04:45 +0000

    Update 3 files

---
+++ src/components/card/cardImpressum.component.jsx
@@ -0,0 +1,12 @@
+
+
+const Impressum = ({title, }) => (
+  <div>
+    <h1>{title}</h1>
+    <p>Name: Max Mustermann</p>
+    <p>Adresse: Musterstraße 123, 12345 Musterstadt</p>
+    <p>E-Mail: max.mustermann@example.com</p>
+  </div>
+);
+
+export default Impressum;
